9ae5a36c17132056e4781bbc948573fba0d03447
STUDENT ELisha Afaayo <ELisha.Afaayo@students.ittralee.ie>
2023-11-30 10:52:56 +0000

    Signed-off-by: STUDENT ELisha Afaayo <ELisha.Afaayo@students.ittralee.ie>

--- My First Game/Assets/EdControl.cs
+++ My First Game/Assets/EdControl.cs
@@ -2,19 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EdControls : MonoBehaviour
+public class EdControl : MonoBehaviour
 {
 
-    Animator Edanimatoins   ;
+    Animator edcontroller;
     // Start is called before the first frame update
     void Start()
     {
-        
+        edcontroller = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetMouseButton(0))
+        {
+            edcontroller.SetBool("shouldWave", true);
+        }
     }
 }
